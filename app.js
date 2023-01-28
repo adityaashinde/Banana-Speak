1da2b339fa7ed3eb27fe53e471fdb692c38302e8
Aditya Shinde <adityashinde4580@gmail.com>
2023-01-28 21:07:49 +0530

    feat: app.js

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-console.log("Hello:) Script is working properly");
+const textInput = document.querySelector("#txt-input");
+const output = document.querySelector("#output");
+const form = document.querySelector("#form");
 
-// var username = prompt ("Give me username")
-// alert("This scripts works!" + username);
+let URL = "https://api.funtranslations.com/translate/minion.json";
+
+const getURL = (text) => `${URL}?text=${text}`;
+
+const errorHandler = (error) => alert(`Error occured: ${error}`);
+
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+
+const inputText = textInput.value;
+
+fetch(getURL(inputText))
+.then((response) => response.json())
+.then((json) => (output.innerText = json.contents.translated))
+.catch(errorHandler);
+});
